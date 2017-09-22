fc47c5043747641748badce91a7f2a37c8729975
Joao Victor Cabral <jgazbral@gmail.com>
2017-09-22 10:42:55 -0300

    Terminado o que foi pedido

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,11 +1,13 @@
 from django.conf.urls import include, url
 from django.contrib.auth import views as auth_views
-from views import register, dashboard
+from views import register, dashboard, edit, edit_password
 
 
 urlpatterns =[
     url(r'^$', dashboard,  name='dashboard'),
     url(r'^cadastrar/$', register,  name='register'),
+    url(r'^editar/$', edit , name='edit'),
+    url(r'^editar-senha/$', edit_password , name='edit_password'),
     url(r'^entrar/$', auth_views.login, {'template_name' : 'login.html'}, name='login'),
     url(r'^sair/$', auth_views.logout, {'next_page' : 'core:home'}, name='logout'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -2,14 +2,16 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, PasswordChangeForm
 from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
 from django.conf import settings
 
-from forms import RegisterForm
+from forms import RegisterForm, EditAccountForm
 
+@login_required
 def dashboard(request):
-    template_name = '/dashboard.html'
+    template_name = 'dashboard.html'
     return render(request, template_name)
 
 def register(request):
@@ -30,5 +32,34 @@ def register(request):
     }	
     return render(request, template_name,context)
 
+@login_required
+def edit(request):
+    template_name = 'edit.html'
+    form = EditAccountForm()
+    context = {}
+    if request.method == 'POST':
+        form = EditAccountForm(request.POST, instance=request.user)
+        if form.is_valid():
+            form.save()
+            form = EditAccountForm(instance=request.user)
+            context['success'] = True
+    else:
+        form = EditAccountForm(instance=request.user)
+    context['form'] = form
+    return render(request, template_name, context)
+
+@login_required
+def edit_password(request):
+    template_name = 'edit_password.html'
+    context = {}
+    if request.method == 'POST':
+        form = PasswordChangeForm(data = request.POST, user = request.user)
+        if form.is_valid():
+            form.save()
+            context['success'] = True
+    else:
+        form = PasswordChangeForm(user = request.user)
+    context['form'] = form
+    return render(request, template_name, context)
 
 # Create your views here.
